e804ff8eaf8e2f81d5aad3752a5c4a2689082311
Yvonne60-bit <55554608+Yvonne60-bit@users.noreply.github.com>
2019-10-10 15:48:47 -0400

    Create signal.c

---
+++ signal.c
@@ -0,0 +1,22 @@
+/* hello_signal.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handler(int signum)
+{ //signal handler
+  printf("Hello World!\n");
+  //exit(1); //exit after printing
+  printf("turning was right\n");
+  alarm(1); 
+    
+}
+
+int main(int argc, char * argv[])
+{
+  signal(SIGALRM,handler); //register handler to handle SIGALRM
+  alarm(1); //Schedule a SIGALRM for 1 second
+  while(1); //busy wait for signal to be delivered
+  return 0; //never reached
+}
